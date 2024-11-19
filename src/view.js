efac692179bc68df16b21f41f7a7112328f61e3d
Mikhail Kos <him182@yandex.ru>
2024-11-19 19:37:10 +0300

    4

--- src/view.js
+++ src/view.js
@@ -7,7 +7,10 @@ import axios from 'axios';
 
 const formEl = document.querySelector('form');
 const inputEl = document.querySelector('#url-input');
-const butEl = document.querySelector('button');
+// const butEl = document.querySelector('button');
+const pEleemFeedback = document.querySelector('.feedback')
+
+
 
 const divElemF = document.createElement('div');
 const h2ElemF = document.createElement('h2');
@@ -15,8 +18,12 @@ h2ElemF.textContent = 'Фиды';
 const ulElemF = document.createElement('ul');
 const liElemF = document.createElement('li');
 
+
 const divElemFirst = document.createElement('div');
 const ulElem = document.createElement('ul');
+
+ulElem.setAttribute('class', 'list-group border-0 rounded-0')
+
 const divElemSecond = document.createElement('div');
 const divElemLast = document.createElement('div');
 const h2Elem = document.createElement('h2');
@@ -25,65 +32,122 @@ divElemSecond.setAttribute('class', 'card-border-0');
 divElemLast.setAttribute('class', 'card-body');
 h2Elem.textContent = 'Посты';
 
+// Как можно исправить?___________________________________________________________________
+const feedbackErr = (formData) => {
+  if (formData.err) {
+    pEleemFeedback.setAttribute('class', 'feedback m-0 position-absolute small text-success')
+  } else {
+    pEleemFeedback.setAttribute('class', 'feedback m-0 position-absolute smail text-danger')
+  }
+  
+}
+// Как можно исправить?____________________________________________________________________
 
 
 const formData = {
   website: '', 
-    posts: {
+  posts: {
     title: [],
     descr: '',
     item: [],
     str: [],
-    fid: []
+    fid: [],
+    flowWebsite:[],
+    itemDescription: []
  },
   validate: '',
+  err: '',
+  modal: [],
+  dubl: []
 }
-// console.log('FORMAFORMA!!!!!!!',formData)
 
-const watchedObj = onChange(formData, () => {});
+
+const watchedObj = onChange(formData, (a, b, c, d) => {
+  console.log('66',{ a, b, c, d })
+  
+  
+});
 
 
 setLocale({
   mixed: {
-    required: ({ required }) => ({ key: inputEl.nextElementSibling.textContent = i18next.t('err.req') , values: { required } }),
+    required: ({ required }) => ({ key: pEleemFeedback.textContent = i18next.t('err.req') , values: { required } }),
   },
   string: {
-    url: ({ url }) => ({ key: inputEl.nextElementSibling.textContent = i18next.t('err.url') , values: { url } }),
+    url: ({ url }) => ({ key: pEleemFeedback.textContent = i18next.t('err.url') , values: { url } }),
+    notOneOf: 'err',
+    // notOneOf: ({ notOneOf }) => ({ key: pEleemFeedback.textContent = i18next.t('err.ebb') , values: { notOneOf } }),
   },
 });
 
-let schema = yup.object().shape({
-  website: yup.string().required().url().nullable().matches()
+// console.log('77', formData)
+
+let schema = yup.object({
+  website: yup.string().notOneOf(formData.dubl)
 });
 
-formEl.addEventListener('submit', async (even) => {
-  even.preventDefault();
-  formData.website = even.target[0].value;
-  const isValid = await schema.isValid(formData);
 
-  watchedObj.validate = isValid;
-  watchedObj.website = inputEl.value;
 
-  console.log('isValid',isValid);
-  console.log('formData',formData);
+formEl.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  
+  // console.log('90', JSON.stringify(formData, null, 2))
+  const newWebsite = event.target[0].value;
+  // const isValid = await schema.isValid(formData);
+  // console.log('99', isValid)
+
+  watchedObj.dubl.push(newWebsite);
+  watchedObj.website = newWebsite;
+  // console.log('94', JSON.stringify(formData, null, 2))
+
+  // const isValid = schema.isValidSync(watchedObj);
+
+
+  
+
+  
+
+ 
+ 
+  // const isValid = await schema.isValid(formData);
+  
+  // watchedObj.validate = isValid;
+  // watchedObj.website = inputEl.value;
+
+
+
 
   try {
-    await schema.validate({website: watchedObj.website});
+    const result = await schema.validate(formData);
+    console.log('120', result);
   } catch (err) {
+    
   err.errors.map((err) => i18next.t(err.key));
+  watchedObj.err = false // Как можно исправить?
+  feedbackErr(formData) // Как можно исправить?
   }
-  if(isValid) requestRRS(formData)
-  
+  // if(isValid) {
+  //   requestRRS(formData.website)
+  //   pEleemFeedback.textContent = i18next.t('err.add')
+  //   watchedObj.err = true // Как можно исправить?
+  //   formEl.reset()
+  //   feedbackErr(formData) // Как можно исправить?
+
+  // }
 })
 
 
 
 
-const requestRRS = (obj) => {
-  console.log('obj.website AXIOS',obj.website)
-  axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(obj.website)}`)
+
+
+const requestRRS = (website) => {
+  console.log('obj.website AXIOS',website)
+  axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(website)}`)
   .then(function (response) {
     console.log('response',response)
+
     parsingRRS(response)
   })
   .catch(function (error) {
@@ -96,25 +160,39 @@ const parsingRRS = (response) => {
   const domElem = parser.parseFromString(response.data.contents, "image/svg+xml");
   // console.log('ПАРСИМ TITLE', domElem.querySelector('title'))//ul li h3 
   // console.log('ПАРСИМ description', domElem.querySelector('description')) //p
-
+ 
   watchedObj.posts.title = domElem.querySelector('title');
   watchedObj.posts.descr = domElem.querySelector('description');
   watchedObj.posts.item = domElem.querySelectorAll('item');
+  watchedObj.posts.itemDescription = domElem.querySelectorAll('item description')
+
+
+
+
+  watchedObj.posts.flowWebsite.push(formData.website)
   filterDom(formData)
   // fidElem(formData);
-  
+  // postElem(formData)
 }
 
 const filterDom = (formData) => {
-  
-  
+  const testProw = formData.posts.fid
+
+  console.log('ЧТО В ИНДЕКСЕ',testProw[0])
   const titleElem = formData.posts.title;
-  // console.log('PROVERKA', titleElem)
+  // console.log('PROVERKA', formData.posts.item)
   const itemElem = formData.posts.item;
-  if(formData.posts.str.length === 0) {
+  if(testProw.length === 0) {
     postElem(formData)
-    fidElem(formData) 
-  } else {
+    fidElem(formData)
+    
+    
+  } 
+  
+
+
+  else {
+    
     itemElem.forEach(element => {
       const titleE = element.querySelector('title');
       const test =  formData.posts.str
@@ -126,20 +204,19 @@ const filterDom = (formData) => {
       }
     })
   }
-  
-  
 }
 
 
 
 
 const fidElem = (formData) => {
-  
-
-  
+  const testProw = formData.posts.fid
   const titleElem = formData.posts.title;
   const descrElem = formData.posts.descr;
-  
+  if (testProw.includes(titleElem.textContent)) {
+
+    return
+  }
   
   const h3ElemF = document.createElement('h3');
   h3ElemF.textContent = titleElem.textContent;
@@ -151,44 +228,153 @@ const fidElem = (formData) => {
   divElemF.append(h2ElemF);
   divElemF.append(ulElemF);
   const fidE = document.querySelector('#rowFid');
-
-  watchedObj.posts.fid.push(titleElem.textContent)
+  
+  
  
   fidE.append(divElemF);
+  watchedObj.posts.fid.push(titleElem.textContent)
+  // flowCheck(formData.posts.flowWebsite)
 }
-
-
+let idIndex = -1;
 
 const postElem = (formData) => {
   const itemElem = formData.posts.item;
-
   const rowURLDiv = document.querySelector('#rowURL');
 
+  
+  
+  
   divElemLast.append(h2Elem);
   divElemSecond.append(divElemLast);
   divElemSecond.append(ulElem);
   divElemFirst.append(divElemSecond);
   rowURLDiv.append(divElemFirst);
-
+  
     itemElem.forEach(element => {
       const linkE = element.querySelector('link');
       const titleE = element.querySelector('title');
-
+      const dickE = element.querySelector('description');
+      
+      // console.log('ЧТО В СТРОКЕ dickE',dickE)
+      
+      
+      
+      if (formData.posts.str.includes(titleE.textContent)) {
+        return
+      }
+      
+      idIndex += 1;
       const aE = document.createElement("a");
       const liE = document.createElement("li");
+      const butE = document.createElement('button')
+      
+      liE.setAttribute("class", "list-group-item d-flex justify-content-between align-items-start border-0 border-end-0")
 
+      butE.setAttribute("type", "button")
+      butE.setAttribute("class", "btn btn-outline-primary btn-sm")
+      butE.setAttribute("data-bs-toggle", "modal")
+      butE.setAttribute("data-bs-target", "#modal")
+      butE.setAttribute("data-id", idIndex)
+      butE.textContent = "Просмотр"
+      
+
+
+
+      
+      // console.log('modal сылка',linkE.textContent)
+      // console.log('modal индекс', idIndex)
+      // console.log('modal заголовок',titleE.textContent)
+      // watchedObj.modal.push({id: idIndex, link: linkE.textContent, title: titleE.textContent, dick: dickE.textContent})
+      
       aE.setAttribute("href", linkE.textContent);
+      aE.setAttribute("class", "fw-bold")
+      aE.setAttribute("target", "_blank")
+      aE.setAttribute("rel", "noopener noreferrer")
+      aE.setAttribute("data-id", idIndex)
+      
+
       aE.textContent = titleE.textContent;
+//___________________________________________________________modal
+// butE.addEventListener('click', (t) => {
+//   if (t.getAttribute("data-id") === 1) {
+//     console.log("EBAAATTTATATATATTATATAT")
 
+//   }
+// })
+      // console.log('KNOPKA', butE.getAttribute("data-id"))
+
+      // const modalh5Title = document.querySelector(".modal-title")
+      // modalh5Title.textContent = titleE.textContent
+
+      // const modalHref = document.querySelector(".btn")
+      // modalHref.setAttribute("href", linkE.textContent)
+
+      // const modalID = document.querySelector(".modal")
+      // modalID.setAttribute("id", idIndex)
+
+      
+//___________________________________________________________
       watchedObj.posts.str.push(titleE.textContent)
-
       liE.append(aE);
       ulElem.append(liE);
+      liE.append(butE)
+      
+      watchedObj.modal.push({id: idIndex, link: linkE.textContent, title: titleE.textContent, dick: dickE.textContent})
+
+      
     })
-    
 }
 
 
+const modal = (formData) => {
+  const exampleModal = document.getElementById('modal')
+  if (exampleModal) {
+    
+    exampleModal.addEventListener('show.bs.modal', event => {
+      
+      const button = event.relatedTarget
+      
+      
+     
+      
+      const recipient = button.getAttribute('data-id')
+      const modalTitle = exampleModal.querySelector('.modal-title')
+      const modalBodyInput = exampleModal.querySelector('.modal-body')
+      const modalbuten = exampleModal.querySelector(".btn")
+  
+      const modal = formData.modal[recipient]
+      console.log('MODALLL!!!!!!!!',modal)
+      modalTitle.textContent = modal.title
+      modalbuten.setAttribute("href", modal.link)
+      modalBodyInput.textContent = modal.dick
+    })
+  }
+  }
+  modal(formData)
+  
+  
+
+
+// const flowCheck = (flows) => {
+  
+//   // console.log('ЧТО ТУТ flowsflows ПРОИСХОДИТ!!!',flows)
+//   flows.forEach(flow => {
+//     console.log('ЧТО ТУТ flowsflows ПРОИСХОДИТ!!!',flow)
+//     requestRRS(flow)
+//   })
+
+
+// setTimeout(flowCheck, 5000, formData.posts.flowWebsite)
+// }
+
+
+
+
+
+
+
+// const proba = document.querySelector(".modal")
+// console.log('ЧТО ТУТУ МЕНЯ ',proba)
 
 
 // const postElem = (formData) => {
